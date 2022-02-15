62e1cdea69c2c96a0994fec61d59411a3b470358
Maksim Negoluk <maksim.negoluk2003@gmail.com>
2022-02-15 17:07:03 +0200

    added autoprefixer in webpack

--- task1.7/webpack.config.js
+++ task1.7/webpack.config.js
@@ -1,57 +1,58 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractorPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractorPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-    node: false,
-    output: {
-        assetModuleFilename: "assets/[hash][ext][query]",
-        clean: true,
-    },
-    plugins: [
-        new MiniCssExtractorPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })],
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
+  node: false,
+  output: {
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true,
+  },
+  plugins: [
+    new MiniCssExtractorPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [autoprefixer()],
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractorPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["postcss-preset-env", {}]],
+              },
             },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractorPlugin.loader,
-                    "css-loader",
-                    {
-                        loader: "postcss-loader",
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    [
-                                        "postcss-preset-env",
-                                        {
-
-                                        }
-                                    ],
-                                ],
-                            },
-                        },
-                    },
-                    "sass-loader"
-                ],
-
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource"
-            },
+          },
+          "sass-loader",
         ],
-    }
-}
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
